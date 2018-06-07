9f5bf60ab55ad49e09cc50326d4885f9b4598c2e
Jefferson Santos <jeffersonsantos@brq.com>
2018-06-07 13:42:40 -0300

    Initial

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './img/favicon.png';
+//import './css/pure-min.css';
+//import './css/index.css';
+
+ReactDOM.render(
+    <h1>Bem Vindo ao React</h1>,
+    document.querySelector("#main")
+)
---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const webpack = require('webpack');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+plugins = [
+    new HtmlWebpackPlugin({
+        filename: 'index.html',
+        template: path.join(__dirname, 'src/index.html')
+    }),
+    new ExtractTextPlugin('style.css'),
+];
+
+module.exports = {
+    entry: path.join(__dirname, 'src/index.jsx'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    resolve: {
+        extensions: [".js",".jsx"]
+    },
+    plugins: plugins,
+
+    module: {
+        rules: [
+            {
+                test: /.jsx?$/,
+                exclude: /node_modules/,
+                include: path.join(__dirname, 'src'),
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: ['env', 'react']
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.(jpe?g|ico|png|gif|svg)$/i,
+                loader: 'file-loader?name=img/[name].[ext]'
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            }
+        ]
+        
+    },
+    devServer: {
+        publicPath: '/',
+        contentBase: './dist'
+    }
+};
