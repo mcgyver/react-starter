618ced517c9cc4f9a19bb7c05905ee1b3c0fbae5
Jefferson Santos <jeffersonsantos@brq.com>
2018-06-08 09:21:33 -0300

    Improve to pass in lighthouse

--- src/sw.js
+++ src/sw.js
@@ -1,5 +1,24 @@
-function instalarServiceWorker() {
-    console.log('ServiceWorker instalado com sucesso!');
-}
+self.addEventListener("install", (event) => {
+    event.waitUntil(
+        caches.open("starter").then((cache) => {
+            return cache.addAll([
+                '/bundle.js',
+                '/img/favicon.png',
+                '/img/icon-72x72.png',
+                '/img/icon-96x96.png',
+                '/img/icon-128x128.png',
+                '/img/icon-144x144.png',
+                '/img/icon-152x152.png',
+                '/img/icon-192x192.png',
+            ]);
+        })
+    );
+});
 
-self.addEventListener("install", instalarServiceWorker);
+self.addEventListener('fetch', (event)  => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
